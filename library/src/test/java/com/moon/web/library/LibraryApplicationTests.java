bd346e9618414714b62a3384f87f647e7921ef3d
문경미 <cher1605@newfrom.net>
2024-01-29 14:57:35 +0900

    init

---
+++ library/src/test/java/com/moon/web/library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moon.web.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
